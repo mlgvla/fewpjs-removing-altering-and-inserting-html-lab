f4aa516ee4267315851e1e16ba2ee41123e6d470
mlgvla <mlgerardvla@gmail.com>
2021-07-06 23:51:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+let body = document.querySelector('body')
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = "Monica is the champion"
+
